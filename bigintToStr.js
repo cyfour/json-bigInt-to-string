dadaf184370452090afc7d2a0315065a1ffb60a2
cy123 <123689241@qq.com>
2019-03-25 09:04:12 +0800

    updated

--- bigintToStr.js
+++ bigintToStr.js
@@ -5,14 +5,9 @@ var bigintToStr=function(json,min,max){
         alert("JSON string error")
         return false;
     }
-    var par=new RegExp("[: ]{1}[\\d]{"+min+","+max+"}[\\r\\n},]{1}","g");
-    var match_arr = json.match(par);
+    var par=new RegExp("([: ]{1})([\\d]{"+min+","+max+"})([\\r\\n},]{1})","g");
     var result=json;
-    if(match_arr){
-        match_arr.forEach(d=>{
-            result=json.replace(d,d[0]+'"'+d.slice(1,d.length-1)+'"'+d[d.length-1]);
-        })
-    }
-    return result;
+    result=result.replace(par,'$1"$2"$3');
+    return result
 }
 
