e269caaaa35ae680307c282b9eea74177e63b78a
chenyong <123689241@qq.com>
2022-03-07 14:55:17 +0800

    update

--- bigintToStr.js
+++ bigintToStr.js
@@ -1,13 +1,21 @@
-var bigintToStr=function(json,min,max){
+var bigintToStr=function(json){
     try {
         JSON.parse(json)
     }catch (e) {
         alert("JSON string error")
         return false;
     }
-    var par=new RegExp("([: ]{1})([\\d]{"+min+","+max+"})([\\r\\n},]{1})","g");
     var result=json;
-    result=result.replace(par,'$1"$2"$3');
-    return result
+    var par = /([^\\]{1}[":]{2}[ \[]{0,2})([\d]{16,})([\r\n},\]]{1})/g;
+    const arrReg = /\[((\d,)*\d)*\]/g;
+    result = result.replace(arrReg, (match) => {
+        if (match.length > 2) {
+            return match.charAt(0) + match.slice(1, match.length - 1).replace(/([\d]{16,})/g, '"$1"') + match.charAt(match.length - 1)
+        } else {
+            return match
+        }
+    })
+    result = result.replace(par, '$1"$2"$3');
+    return result;
 }
 
